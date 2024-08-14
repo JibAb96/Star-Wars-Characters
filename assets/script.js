0d4f6e865c25d15a29b3cfd669d0397713e13a7d
JibAb96 <j.abshir96@gmail.com>
2024-08-14 07:43:54 +0000

    API is now accessible

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,24 @@
-const fetchInnerData = async (num) => {
-    const swapi = "https://swapi.py4e.com/api/people/";
+const name = document.getElementById("star-wars-name");
+const dob = document.getElementById("birthyear");
+const swapi = "https://swapi.py4e.com/api/people";
+const height = document.getElementById("height");
+const weight = document.getElementById("weight");
+const input = document.getElementById("search-input");
+const searchButton = document.getElementById("search-button")
+const fetchData = async (num) => {
     try {
         const response = await fetch(`${swapi}/${num}`);
-        const data = response.json();
-        const name = document.getElementById("star-wars-name");
-        const dob = document.getElementById("birthyear");
+        const data = await response.json();
         name.innerText = `Name: ${data.name}`
         dob.innerText = `Date Of Birth: ${data["birth_year"]}`
-        const height = document.getElementById("height")
-        const weight = document.getElementById("weight")
         weight.innerText = `Weight: ${data.mass}`
         height.innerText = `Height: ${data.height}`
-    } 
+    }
     catch {
-
+        alert("Star Wars Character not found")
     }
 }
+
+searchButton.addEventListener("click", () => {
+    fetchData(input.value)
+})
